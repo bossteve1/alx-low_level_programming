1abcb0020c83c75de15aacb82728f66074bd49ba
bossteve1 <bossteve98@gmail.com>
2022-06-07 13:12:16 -0700

    README.md

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - my entry point
+ * main - prints putchar
  *
  * Description:programme that prints _putchar
- * Return:0
+ * Return: always 0
  */
 int main(void)
 {
